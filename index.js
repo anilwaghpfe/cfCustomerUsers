3603a78f74cd0b27607e7e7af9ee13ef88469beb
Waghmare <waghmaa@amrl2tw07hhtdh.pfizer.com>
2019-04-02 22:16:19 -0400

    1

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 require('./config');
 var _CustomerUsers = require('./CustomerUsersRoute');
-const language = require('@google-cloud/language');
-const client = new language.LanguageServiceClient();
+// const language = require('@google-cloud/language');
+// const client = new language.LanguageServiceClient();
 const express = require('express');
 const app = express();
 
 app.use('/CustomerUsers/', _CustomerUsers);
 
 // Expose the API as a function
-exports.api = functions.https.onRequest(app);
+// exports.api = functions.https.onRequest(app);
